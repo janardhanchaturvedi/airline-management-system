050ecde446246e201dbbf03f2cd009c959c48deb
Janardhan <jan.cj84@gmail.com>
2024-12-28 23:56:12 +0530

    Added Basic airport model and migration withouth foreign constraint

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const { AirplaneController } = require("../../controllers");
+const { AirplaneMiddlewares } = require("../../middlewares");
 
 const router = express.Router();
 
 router
-  .post("/", AirplaneController.createAirplane)
+  .post(
+    "/",
+    AirplaneMiddlewares.validateCreateRequest,
+    AirplaneController.createAirplane
+  )
   .get("/", AirplaneController.getAirplanes)
   .get("/:id", AirplaneController.getAirplaneById)
   .delete("/:id", AirplaneController.destroyAirplane)
